fa0ab7215182a65763a741e4205b49dd7df136c4
Adrian <adalarcon@uach.mx>
2018-10-03 10:25:41 -0600

    build

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ var server = app.listen(app.get('port'), () => {
   console.log('[app] Express server listening on port ' + server.address().port);
   require('./server/helper/socket')(server);
 });
+
+
+var http = require("http");
+setInterval(function() {
+    http.get("http://cansat.herokuapp.com/");
+}, 300000); // every 5 minutes (300000)
